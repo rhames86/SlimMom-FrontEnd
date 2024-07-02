3c94d4e1bc3b9dce10b7e15e43614b02e8b2b445
Androogs <139003434+Androogs@users.noreply.github.com>
2024-07-02 17:06:00 -0500

    Creo Index.js
    Con importaciones de las rutas que propongo usar en el proyecto

---
+++ assets/Index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import './assets/fonts/fonts.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
