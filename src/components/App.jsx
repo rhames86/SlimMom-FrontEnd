e631c5989b999c53c7208cc4663e06a79cc3d448
Juan Batista <elderjuanbatista@gmail.com>
2024-07-04 17:02:02 -0400

    Prueba de cambios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,54 @@ function App() {
   return (
     <div className="App">
       <h1>Hola Somos el Grupo 1 | SlimMom Project</h1>
+      <h2>Integrantes:</h2>
+      <ul>
+        <li>
+          <b>Alejandro Garcia Salazar</b>
+        </li>
+        <li>
+          <b>Alejandro Largo T</b>
+        </li>
+        <li>
+          <b>Andres Jordan Andres Jordan</b>
+        </li>
+        <li>
+          <b>Eduardo José Maya Rodriguez</b>
+        </li>
+        <li>
+          <b>Francis Ortega</b>
+        </li>
+        <li>
+          <b>Jessica Gargurevich</b>
+        </li>
+        <li>
+          <b>Johan Devia</b>
+        </li>
+        <li>
+          <b>Jonathan Pabón</b>
+        </li>
+        <li>
+          <b>Juan Batista</b>
+        </li>
+        <li>
+          <b>Juan Urbina</b>
+        </li>
+        <li>
+          <b>Leider Páez</b>
+        </li>
+        <li>
+          <b>Mayra Alejandra Madera</b>
+        </li>
+        <li>
+          <b>Nicolas Camelo Gomez</b>
+        </li>
+        <li>
+          <b>Nicolas Canales Diaz</b>
+        </li>
+        <li>
+          <b>Sandra Echeverri</b>
+        </li>
+      </ul>
     </div>
   );
 }
